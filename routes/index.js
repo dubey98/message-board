dbe16cbc763b0403fb3070eb6e7554e021871a03
shiv dubey <dubeyshivprasad98@gmail.com>
2020-06-25 20:55:59 +0530

    display sample data and basic date formatting added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const moment = require("moment");
+
+const messages = [
+  {
+    text: "Everything's Good!!",
+    user: "shiv",
+    added: new Date(),
+    formatted_date: function () {
+      return moment(this.added).format("DD MMMM YYYY");
+    },
+  },
+  {
+    text: "You are awesome!",
+    user: "george",
+    added: new Date(),
+    formatted_date: function () {
+      return moment(this.added).format("DD MMMM YYYY");
+    },
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "MIni Messageboard", messages: messages });
 });
 
 module.exports = router;
